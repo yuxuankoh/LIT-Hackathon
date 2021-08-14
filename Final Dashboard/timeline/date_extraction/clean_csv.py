41c1dfbf176c91c5686b57da360f7d0a45a63391
kwmiw <pennypolarbear@gmail.com>
2021-08-14 20:56:44 +0800

    Create clean_csv.py

---
+++ Final Dashboard/timeline/date_extraction/clean_csv.py
@@ -0,0 +1,12 @@
+import csv
+from datetime import datetime
+
+# with open('../../timeline_csvs/timeline_food.csv') as csv_data:
+# with open('../../timeline_csvs/timeline_hotel.csv') as csv_data:
+with open('../../timeline_csvs/timeline_retail.csv') as csv_data:
+    reader = csv.reader(csv_data, delimiter=',')
+
+    # sort csv with date
+    days_sorted = sorted(reader, key=lambda day: datetime.strptime(day[0], "%d/%m/%Y"), reverse=True)
+    print(days_sorted)
+    csv_data.close() 
